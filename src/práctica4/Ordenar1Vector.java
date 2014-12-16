b0f2fb4a8236d821b9ca7d3a8f3ccddc73e2db8e
Aythami <Aythami@Aythami-VAIO>
2014-12-16 20:57:41 +0000

    Versión 1

---
+++ src/práctica4/Ordenar1Vector.java
@@ -0,0 +1,14 @@
+package práctica4;
+
+public class Ordenar1Vector implements OrdenarVector {
+    private final String nombreMetodo="";
+
+    @Override
+    public String nombreMetodo() {
+        return nombreMetodo;
+    }    
+
+    @Override
+    public void ordena(int[] v, int n) {
+    }
+}
---
+++ src/práctica4/OrdenarVector.java
@@ -0,0 +1,6 @@
+package práctica4;
+
+public interface OrdenarVector {
+    public String nombreMetodo();
+    public void ordena(int[] v, int n);
+}
